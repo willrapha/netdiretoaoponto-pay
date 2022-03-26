f7321258a75ca4b1b42ac907ac0f69f71fc0ad19
Willian Raphael <willianr.matos@outlook.com>
2022-03-26 16:19:37 -0400

    Micro Pagamentos

---
+++ DevFreela.Payments.API/Controllers/PaymentsController.cs
@@ -0,0 +1,30 @@
+﻿using DevFreela.Payments.API.Models;
+using DevFreela.Payments.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DevFreela.Payments.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class PaymentsController : ControllerBase
+    {
+        private readonly IPaymentService _paymentService;
+
+        public PaymentsController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        // POST api/payments
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] PaymentInfoInputModel paymentInfoInputModel)
+        {
+            var result = await _paymentService.ProcessPayment(paymentInfoInputModel);
+
+            if (!result)
+                return BadRequest();
+
+            return NoContent();
+        }
+    }
+}
---
+++ DevFreela.Payments.API/Services/PaymentService.cs
@@ -0,0 +1,13 @@
+﻿using DevFreela.Payments.API.Models;
+using System.Threading.Tasks;
+
+namespace DevFreela.Payments.API.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        public async Task<bool> ProcessPayment(PaymentInfoInputModel paymentInfoInputModel)
+        {
+            return true;
+        }
+    }
+}
